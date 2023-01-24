bd07423409fb41d5e1d51ad6087937cf2e21a822
Alec Gunny <alec.gunny@gmail.com>
2023-01-24 12:00:22 -0800

    cleaning up notebook and running end-to-end

--- utils/tracker.py
+++ utils/tracker.py
@@ -1,7 +1,7 @@
 import pickle
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Generator, Iterable, Optional, TypeVar
+from typing import Dict, Generator, Iterable, List, Optional, TypeVar
 
 import torch
 from rich.console import Console
@@ -20,6 +20,37 @@ console = Console()
 T = TypeVar("T")
 
 
+class MultiThresholdAUROC(torch.nn.Module):
+    """Not using this for now, but saving for reference
+
+    Computes the AUC of a set of foreground and background
+    predictions up to several different maximum fpr levels.
+    """
+
+    def __init__(self, thresholds: List[float]):
+        super().__init__()
+        thresholds = torch.Tensor(thresholds)
+        self.register_buffer("thresholds", thresholds)
+
+    def forward(self, signal_preds, background_preds):
+        x = torch.cat([signal_preds, background_preds])
+        y = torch.zeros_like(x)
+        y[: len(signal_preds)] = 1
+
+        idx = torch.argsort(x, descending=True)
+        y = y[idx]
+
+        tpr = torch.cumsum(y, -1) / y.sum()
+        fpr = torch.cumsum(1 - y, -1) / (1 - y).sum()
+        dfpr = fpr.diff()
+        dtpr = tpr.diff()
+
+        mask = fpr[:-1, None] <= self.thresholds
+        dfpr = dfpr[:, None] * mask
+        integral = (tpr[:-1, None] + dtpr[:, None] * 0.5) * dfpr
+        return integral.sum(0)
+
+
 class ThroughputColumn(ProgressColumn):
     def render(self, task):
         N = task.completed * task.fields["batch_size"]
@@ -28,12 +59,19 @@ class ThroughputColumn(ProgressColumn):
 
 
 class Run:
-    def __init__(self, model: torch.nn.Module, output_directory: Path) -> None:
+    def __init__(
+        self,
+        model: torch.nn.Module,
+        output_directory: Path,
+        auroc_metric: MultiThresholdAUROC,
+    ) -> None:
         self.output_directory = output_directory
         self.model = model
         self.max_epochs = None
 
         self.checkpoint_dir.mkdir(parents=True, exist_ok=True)
+        self.best_train_loss = self.best_valid_loss = float("inf")
+        self.history = {"train_loss": [], "valid_loss": [], "auroc": {}}
 
         self.layout = Layout(name="root")
         self.layout.split(
@@ -54,11 +92,17 @@ class Run:
         self.table.add_column("Train Loss")
         self.table.add_column("Valid Loss")
 
+        colors = "red,magenta,yellow,bright_cyan,dark_orange3".split(",")
+        self.best_aurocs, self.colors = {}, {}
+        for i, fpr in enumerate(auroc_metric.thresholds.cpu().numpy()):
+            fpr = f"{fpr:0.2f}"
+            self.table.add_column(f"AUC@{fpr}")
+            self.history["auroc"][fpr] = []
+            self.best_aurocs[fpr] = 0
+            self.colors[fpr] = colors[i]
+
         self.layout["progress"].update(self.prog)
         self.layout["table"].update(self.table)
-
-        self.best_train_loss = self.best_valid_loss = float("inf")
-        self.history = {"train_loss": [], "valid_loss": []}
 
     @property
     def checkpoint_dir(self) -> Path:
@@ -68,7 +112,9 @@ class Run:
     def best_weights_path(self) -> Path:
         return self.output_directory / "training" / "weights.pt"
 
-    def update(self, train_loss: float, valid_loss: float) -> None:
+    def update(
+        self, train_loss: float, valid_loss: float, aurocs: Dict[float, float]
+    ) -> None:
         self.history["train_loss"].append(train_loss)
         self.history["valid_loss"].append(valid_loss)
 
@@ -85,6 +131,17 @@ class Run:
         else:
             valid_color = ""
 
+        auroc_rows = []
+        for fpr, auroc in aurocs.items():
+            fpr = f"{fpr:0.2f}"
+            self.history["auroc"][fpr].append(auroc)
+            if auroc >= self.best_aurocs[fpr]:
+                self.best_aurocs[fpr] = auroc
+                color = f"[{self.colors[fpr]}]"
+            else:
+                color = ""
+            auroc_rows.append(f"{color}{auroc:0.3e}")
+
         epoch = len(self.history["train_loss"])
         str_epoch = str(epoch).zfill(3)
         if not epoch % 5:
@@ -99,6 +156,7 @@ class Run:
             epoch_row,
             f"{train_color}{train_loss:0.3e}",
             f"{valid_color}{valid_loss:0.3e}",
+            *auroc_rows,
         )
 
         self.layout["table"].update(self.table)
